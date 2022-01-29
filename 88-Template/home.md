6bd2374fff655929a465e49ec57d960470206cad
cumany <42957010+cumany@users.noreply.github.com>
2022-01-30 00:02:26 +0800

    分栏描述修正

--- 88-Template/home.md
+++ 88-Template/home.md
@@ -108,37 +108,33 @@ dv.paragraph(posters);
 ```
 ````
 
----
 %%便签板块%%
 %%可通过侧边栏主页便签按钮快捷更改便签内容%%
+
+---
 ````ad-flex
-<div>
 <!--notice1-->
-<p class="stickies">
+<p class="stickies" style="max-width:180px" >
 <strong>倒计时</strong></br>今年已过去<strong><%+* tR+= moment().diff(tp.date.now("YYYY-1-1"), "days") %></strong>天
 </br>距春节还有<strong><%+* let edate = moment("2022-02-01", "yyyy-MM-DD"); let from = moment().startOf('day'); edate.diff(from, "days") >= 0 ? tR += edate.diff(from, "days") : tR += edate.add(1, "year").diff(from, "days") %></strong>天
 </p>
 <!--notice2-->
-<p class="stickies2" style="width: max(220px, 30%)" >
-🧨<br>便签二
+<p class="stickies2" style="max-width:200px" >
+🌸<br> 每天一个好心情！！
 </p>
 
-
-</div>
-<div>
 <!--notice3-->
-<p class="stickies">
-这里可以放一些重要内容
+<p class="stickies" style="max-width:200px">
+💖<br> 永远相信美好的事情就要发生！
 </p>
-
 
 <!--notice4-->
-<p class="stickies2" style="width: max(220px, 30%)" >
-🎈<br> 永远相信美好的事情就要发生！
+<p class="stickies2" style="max-width:200px" >
+💌<br> 开启美好的一天
 </p>
 
 <!---->
-</div>
+
 ````
 ---
 
--- 88-Template/script/changeSticky.js
+++ 88-Template/script/changeSticky.js
@@ -6,25 +6,25 @@ module.exports = async function colorclock (params) {
   );
 
   let stickyContent = await params.quickAddApi.inputPrompt("请输入便签内容")
-  const stickyPatter1 = /(?<=1--\>\W)\<p\Wclass="stickies"\>([\w\W]*?)\<\/p\>/
+  const stickyPatter1 = /(?<=1--\>\W)\<p\Wclass="stickies".*\>([\w\W]*?)\<\/p\>/
   const stickyPatter2 = /(?<=2--\>\W)\<p\Wclass="stickies2".*\>([\w\W]*?)\<\/p\>/
-  const stickyPatter3 = /(?<=3--\>\W)\<p\Wclass="stickies"\>([\w\W]*?)\<\/p\>/
+  const stickyPatter3 = /(?<=3--\>\W)\<p\Wclass="stickies".*\>([\w\W]*?)\<\/p\>/
   const stickyPatter4 = /(?<=4--\>\W)\<p\Wclass="stickies2".*\>([\w\W]*?)\<\/p\>/
 
   const filePath = app.vault.adapter.basePath + "\\88-Template\\home.md"
   const fileContent = await app.vault.adapter.fs.readFileSync(filePath, "utf8")
 
   if(pickedSticky == "1" && stickyContent){
-    let newContent = fileContent.replace(stickyPatter1,  `<p class="stickies">\n${stickyContent}\n</p>\n`)
+    let newContent = fileContent.replace(stickyPatter1,  `<p class="stickies" style="max-width:180px" >\n${stickyContent}\n</p>\n`)
     await app.vault.adapter.fs.writeFileSync(filePath, newContent,"utf8")
   }else if(pickedSticky == "2" && stickyContent){
-    let newContent = fileContent.replace(stickyPatter2, `<p class="stickies2" style="width: max(220px, 30%)" >\n${stickyContent}\n</p>\n`)
+    let newContent = fileContent.replace(stickyPatter2, `<p class="stickies2" style="max-width:200px" >\n${stickyContent}\n</p>\n`)
     await app.vault.adapter.fs.writeFileSync(filePath, newContent,"utf8")
   }else if(pickedSticky == "3" && stickyContent){
-    let newContent = fileContent.replace(stickyPatter3, `<p class="stickies">\n${stickyContent}\n</p>\n`)
+    let newContent = fileContent.replace(stickyPatter3, `<p class="stickies" style="max-width:200px">\n${stickyContent}\n</p>\n`)
     await app.vault.adapter.fs.writeFileSync(filePath, newContent,"utf8")
   }else if(pickedSticky == "4" && stickyContent){
-    let newContent = fileContent.replace(stickyPatter4, `<p class="stickies2" style="width: max(220px, 30%)" >\n${stickyContent}\n</p>\n`)
+    let newContent = fileContent.replace(stickyPatter4, `<p class="stickies2" style="max-width:200px" >\n${stickyContent}\n</p>\n`)
     await app.vault.adapter.fs.writeFileSync(filePath, newContent,"utf8")
   }else{
     console.log("pickSticky error!")
